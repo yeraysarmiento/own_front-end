93c8607d4ad74eb8bccc7ed1ed6360cdcaebba2d
ysarmiento <yeray.sarmiento@hotmail.com>
2021-11-29 13:02:31 +0100

    Fixed husky pre-push configuration

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="home"></div>
+  <div class="navigator"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {},
+  name: "Nav",
+  props: {},
 });
 </script>
+
+<style scoped lang="scss"></style>
