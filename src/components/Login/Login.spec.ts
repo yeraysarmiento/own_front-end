591618e1549b2f73b4fcb6b83ed196c804184f98
ysarmiento <yeray.sarmiento@hotmail.com>
2021-12-02 11:12:22 +0100

    Add Logintest

--- src/components/Login/Login.spec.ts
+++ src/components/Login/Login.spec.ts
@@ -1,10 +1,12 @@
 import { mount } from "@vue/test-utils";
 import router from "@/router";
 import Login from "./Login.vue";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/vue";
 
 describe("Given a Login Component", () => {
   describe("When it is rendered", () => {
-    test("Then it should render 2 inputs and a button", () => {
+    test("Then it should render 2 inputs and a button", async () => {
       const password =
         '<input id="password" type="password" placeholder="**********" class="">';
       const username =
@@ -12,7 +14,7 @@ describe("Given a Login Component", () => {
       const button =
         '<button class="button disabled" type="submit" disabled="">Submit</button>';
 
-      const wrapper = mount(Login, {
+      const wrapper = await mount(Login, {
         global: {
           plugins: [router],
         },
@@ -22,6 +24,20 @@ describe("Given a Login Component", () => {
       expect(wrapper.html()).toContain(password);
       expect(wrapper.html()).toContain(username);
       expect(wrapper.html()).toContain(button);
+    });
+  });
+  describe("When the fields are not filled", () => {
+    test("Then it should show a disabled submit button", () => {
+      const wrapper = mount(Login, {
+        global: {
+          plugins: [router],
+        },
+        stubs: ["router-link", "router-view"],
+      });
+
+      const button = wrapper.find("button");
+
+      expect(button).toHaveProperty("isDisabled");
     });
   });
 });
