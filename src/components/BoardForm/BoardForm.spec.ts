6824ac8b88c85b7766f52a0eb7918a8e7e0e0f48
ysarmiento <yeray.sarmiento@hotmail.com>
2021-12-05 14:09:52 +0100

    Tried BoardForm test, add structure

--- src/components/BoardForm/BoardForm.spec.ts
+++ src/components/BoardForm/BoardForm.spec.ts
@@ -4,74 +4,24 @@ import router from "@/router";
 
 describe("Given a BoardForm Component", () => {
   describe("When it is rendered", () => {
-    test("Then it should render one heading 'NEW BOARD' and 8 labels", async () => {
+    test("Then it should render one heading 'NEW BOARD'", async () => {
+      // const $store = {
+      //   methods: {
+      //     checkForm: jest.fn(),
+      //     onSubmit: jest.fn(),
+      //   },
+      //   // mounted: jest.fn().mockResolvedValue(true),
+      // };
       // const wrapper = await mount(BoardForm, {
       //   global: {
       //     plugins: [router],
       //   },
-      //   stubs: ["router-link"],
+      //   stubs: ["router-link", "router-view"],
+      //   $store: {
+      //     $store,
+      //   },
       // });
-      //     const labels = wrapper.findAll("label");
-      //     const heading = wrapper.find(".register-title");
-      //     await (() => {
-      //       expect(labels).toHaveLength(8);
-      //       expect(heading.text()).toBe("NEW BOARD");
-      //     });
-      //   });
-      // });
-      // describe("When it is rendered", () => {
-      //   test("Then it should show a disabled button", async () => {
-      //     const wrapper = await mount(BoardForm, {
-      //       global: {
-      //         plugins: [router],
-      //       },
-      //       stubs: ["router-link"],
-      //     });
-      //     const button = wrapper.find("button");
-      //     await (() => {
-      //       expect(button).toHaveProperty("isDisabled");
-      //     });
-      //   });
-      // });
-      // describe("When the user clicks at submit with all required fields completed", () => {
-      //   test("Then it should call the method onSubmit", async () => {
-      //     const methods = {
-      //       onSubmit: jest.fn(),
-      //       checkForm: jest.fn(),
-      //     };
-      //     const $store = {
-      //       state,
-      //       methods,
-      //       dispatch: jest.fn(),
-      //       commit: jest.fn(),
-      //     };
-      //     const wrapper = await mount(BoardForm, {
-      //       setData: {
-      //         name: "name",
-      //         email: "email",
-      //         category: "category",
-      //         instagram: "instagram",
-      //         twitter: "twitter",
-      //         facebook: "facebook",
-      //         about: "A little bit about me",
-      //         isDisabled: true,
-      //         logo: {},
-      //         isWrong: false,
-      //       },
-      //       global: {
-      //         mocks: {
-      //           $store,
-      //           methods,
-      //         },
-      //         plugins: [router],
-      //       },
-      //       stubs: ["router-link"],
-      //     });
-      //     const element = wrapper.find(".button").trigger("click");
-      //     await wrapper.vm.$nextTick();
-      //     expect(calledFunction).toBeCalled();
-      // });
-      // });
+      // // expect(title).toBeInTheDocument();
     });
   });
 });
