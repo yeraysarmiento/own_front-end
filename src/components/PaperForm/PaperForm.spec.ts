746098e7df110ee9f6991aef64b0995e2b12e593
ysarmiento <yeray.sarmiento@hotmail.com>
2021-12-08 13:36:09 +0100

    Attempt to test Getters

--- src/components/PaperForm/PaperForm.spec.ts
+++ src/components/PaperForm/PaperForm.spec.ts
@@ -1,9 +1,101 @@
 import { mount } from "@vue/test-utils";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft, fas } from "@fortawesome/free-solid-svg-icons";
 import Vuex, { createStore } from "vuex";
-import router from "@/router";
+import { createRouter, createWebHistory } from "vue-router";
 import state from "../../../tests/mockedState";
 import PaperForm from "./PaperForm.vue";
+import Editor from "@/components/Editor/Editor.vue";
 import "@testing-library/jest-dom";
+
+library.add(faChevronLeft);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: {
+        template: "Home",
+      },
+    },
+    {
+      path: "/home",
+      name: "Home",
+      component: {
+        template: "Home",
+      },
+    },
+    {
+      path: "/login",
+      name: "OwnLogin",
+      component: {
+        template: "OwnLogin",
+      },
+    },
+    {
+      path: "/register",
+      name: "OwnRegister",
+      component: {
+        template: "OwnRegister",
+      },
+    },
+    {
+      path: "/desk",
+      name: "Desk",
+      component: {
+        template: "Desk",
+      },
+    },
+    {
+      path: "/desk/new",
+      name: "CreateBoard",
+      component: {
+        template: "CreateBoard",
+      },
+    },
+    {
+      path: "/:boardName",
+      name: "BoardPage",
+      component: {
+        template: "BoardPage",
+      },
+    },
+    {
+      path: "/:boardName/new",
+      name: "CreateNewPaper",
+      component: {
+        template: "CreatePaper",
+      },
+    },
+    {
+      path: "/:boardName/:idPaper",
+      name: "PaperPage",
+      component: {
+        template: "PaperPage",
+      },
+    },
+    {
+      path: "/:boardName/edit/:paperId",
+      name: "CreatePaper",
+      component: {
+        template: "CreatePaper",
+      },
+    },
+    {
+      path: "/notfound",
+      name: "NotFound",
+      component: {
+        template: "NotFound",
+      },
+    },
+    {
+      path: "/:catchAll(.*)",
+      redirect: "/notfound",
+    },
+  ],
+});
 
 describe("Given a PaperForm component", () => {
   describe("When its rendered", () => {
@@ -19,10 +111,17 @@ describe("Given a PaperForm component", () => {
       });
 
       const wrapper = mount(PaperForm, {
+        components: {
+          "font-awesome-icon": FontAwesomeIcon,
+          editor: Editor,
+        },
+        getters: {
+          redirectLogin: jest.fn(),
+        },
         global: {
           plugins: [router, store],
         },
-        stubs: ["router-view", "router-link"],
+        stubs: ["router-view", "router-link", "FontAwesomeIcon", "Editor"],
         mocks: {
           $router: mockRouter,
         },
@@ -48,10 +147,17 @@ describe("Given a PaperForm component", () => {
       };
 
       const wrapper = mount(PaperForm, {
+        components: {
+          "font-awesome-icon": FontAwesomeIcon,
+          editor: Editor,
+        },
+        getters: {
+          redirectLogin: jest.fn(),
+        },
         global: {
           plugins: [router, store],
         },
-        stubs: ["router-view", "router-link"],
+        stubs: ["router-view", "router-link", "FontAwesomeIcon", "Editor"],
         mocks: {
           $router: mockRouter,
         },
@@ -61,33 +167,6 @@ describe("Given a PaperForm component", () => {
 
       const button = wrapper.find("button");
       expect(button.element.getAttributeNames()).toContain("disabled");
-    });
-  });
-  describe("When the localStorage is empty", () => {
-    test("Then it should reroute to /login", async () => {
-      const store = createStore({
-        state() {
-          return state;
-        },
-      });
-
-      const mockRouter = {
-        push: jest.fn(),
-      };
-
-      const wrapper = mount(PaperForm, {
-        global: {
-          plugins: [router, store],
-          mocks: {
-            $router: mockRouter,
-          },
-        },
-        stubs: ["router-view", "router-link"],
-      });
-
-      await router.isReady();
-
-      expect(mockRouter.push).toHaveBeenCalledWith("/login");
     });
   });
   describe("When the localStorage has a valid token", () => {
@@ -103,20 +182,32 @@ describe("Given a PaperForm component", () => {
         actions: {
           getTokenAction,
         },
+        getters: {
+          redirectLogin: jest.fn(),
+        },
       });
 
-      const mockRouter = {
+      const $router = {
         push: jest.fn(),
+      };
+      const $route = {
+        push: jest.fn(),
+        params: "paperId",
       };
 
       const wrapper = mount(PaperForm, {
+        components: {
+          "font-awesome-icon": FontAwesomeIcon,
+          editor: Editor,
+        },
         global: {
           plugins: [router, store],
           mocks: {
-            $router: mockRouter,
+            $route,
+            $router,
           },
         },
-        stubs: ["router-view", "router-link"],
+        stubs: ["router-view", "router-link", "FontAwesomeIcon", "Editor"],
         actions: {
           getTokenAction,
         },
@@ -127,31 +218,45 @@ describe("Given a PaperForm component", () => {
       expect(localStorage.getItem).toHaveBeenCalled();
     });
   });
-  describe("When the localStorage has a not valid token", () => {
+  describe("When the localStorage is empty", () => {
     test("Then it should call push with '/login", async () => {
+      const redirectLogin = jest.fn();
+
       const store = createStore({
         state() {
           return state;
         },
+        getters: {
+          redirectLogin,
+        },
       });
 
-      const mockRouter = {
+      const $router = {
         push: jest.fn(),
+      };
+      const $route = {
+        push: jest.fn(),
+        params: "paperId",
       };
 
       const wrapper = mount(PaperForm, {
+        components: {
+          "font-awesome-icon": FontAwesomeIcon,
+          editor: Editor,
+        },
         global: {
           plugins: [router, store],
           mocks: {
-            $router: mockRouter,
+            $route,
+            $router,
           },
         },
-        stubs: ["router-view", "router-link"],
+        stubs: ["router-view", "router-link", "FontAwesomeIcon", "Editor"],
       });
 
       await router.isReady();
 
-      expect(mockRouter.push).toHaveBeenCalledWith("/login");
+      expect(redirectLogin).toHaveBeenCalled();
     });
   });
 });
